d42521cdfb4806c633e0eb4f7cbe4d3ca1ae98a8
bowook <mbosx316@naver.com>
2023-08-13 00:11:22 +0900

    init

---
+++ Baekjoon10810.py
@@ -0,0 +1,11 @@
+N, M = map(int, input().split())
+
+basket = [0] * N
+
+for a in range(0, M):
+    i, j, k = map(int, input().split())
+    for b in range(i-1, j):
+        basket[b] = k
+
+for i in range(0, N):
+    print(basket[i], end=' ')
